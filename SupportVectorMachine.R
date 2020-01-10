a2486dd6207f77f8bd4e158b5df5ca0efb3b3807
Dom Owens <do16317@bristol.ac.uk>
2020-01-10 09:52:33 +0000

    Add SVM roxygen skeleton

--- SupportVectorMachine.R
+++ SupportVectorMachine.R
@@ -1,6 +1,20 @@
 #Define support vector machine function for multi-dimensional X
 
 
+#' Support Vector Machine classifier
+#'
+#' @param X covariates
+#' @param y response vector
+#' @param max_it maximum iterations
+#' @param eta_0 initial learning rate
+#' @param alpha learning rate decay
+#' @param c cost parameter
+#'
+#' @return numeric vector of coefficient parameter estimates for y
+#' @export
+#'
+#' @examples 
+#' SVM(X = X, y = y, max_it = 1e4, eta_0 = 10, alpha = 0.9, c = 0.9)
 SVM <- function(X, y, max_it = 1e4, eta_0 = 10, alpha = 0.9, c = 0.9){
   
   n <- dim(X)[1] #observations
