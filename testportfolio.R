e24854aab52e91611d6d217836df95cbb9ce6573
Douglas Corbin <dc16890@bristol.ac.uk>
2020-01-16 18:24:12 +0000

    various changes

--- testportfolio.R
+++ testportfolio.R
@@ -1,31 +1,30 @@
 data <- read.csv("data/combined_close_data.csv")
-train <- retrieve.close(data,"2015-01-01","2017-01-01")
-test <- retrieve.close(data,"2017-01-01","2018-01-01")
+train <- retrieve.close(data,"2016-06-01","2017-06-01")
+test <- retrieve.close(data,"2017-06-01","2018-06-01")
 test <- dplyr::select(test,colnames(train))
 train.returns <- returns(train,2)
 
 model <- fit.factor.model(train.returns,10)
 covars <- approx.covar(train.returns,model)
-portfolio <- portfolio.solution(train.returns,model,covars,0.01)
+portfolio <- portfolio.solution(train.returns,model,covars,0.1)
 
 test.returns <- returns(test,2)
 exp.return <- t(portfolio) %*% rowMeans(test.returns)
-port.val <- t(portfolio) %*% t(test[,-1])
+port.val <- t(portfolio) %*% test.returns
 port.val <- port.val * 1/port.val[1]
-plot(1:251,port.val,type="l",ylim = c(1,2.5))
+plot(1:length(port.val),port.val,type="l")
 
-portfolio2 <- optim.portfolio(train.returns,covars$covar)
-portfolio2 <- portfolio2$solution
-exp.return2 <- t(portfolio2) %*% rowMeans(test.returns)
-port.val2 <- t(portfolio2) %*% t(test[,-1])
-port.val2 <- port.val2 * 1/port.val2[1]
-lines(1:251,port.val2,type="l", col = "red")
+t(portfolio) %*% rowMeans(test.returns)
 
-
-portfolio3 <- rep(1/411,411)
+portfolio3 <- rep(1/length(portfolio),length(portfolio))
 exp.return3 <- t(portfolio3) %*% rowMeans(test.returns)
 port.val3 <- t(portfolio3) %*% t(test[,-1])
 port.val3 <- port.val3 * 1/port.val3[1]
-lines(1:251,port.val3,type="l",col = "blue")
+lines(1:length(port.val3),port.val3,type="l",col = "blue")
 
-
+  #Asses model fit.
+test.error <- test.returns - rowMeans(test.returns)%*%t(rep(1,249)) - t(model@loadings) %*% model@scores
+sig0 <- train.returns - rowMeans(train.returns)
+sig0 <- sqrt(apply(sig0,1,var))
+sig1 <- t(model@loadings) %*% model@scores - covars$error
+sig1 <- sqrt(apply(sig1,1,var))
