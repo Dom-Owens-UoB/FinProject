3f8671c730fba9a3ececfb5cb4f3df5a7b286edb
Douglas Corbin <dc16890@bristol.ac.uk>
2020-01-16 18:23:19 +0000

    Corrected error in analytical solution.

--- portopt/R/portfolio_solution.R
+++ portopt/R/portfolio_solution.R
@@ -3,10 +3,10 @@
 #' @param returns A matrix of stock prices
 #' @param model an object of S4 class "model".
 #' @param covars output from the function `approx.covar()`
-#' @param sigma0 upper bound of variance.
+#' @param var upper bound of variance.
 #' @export
 #'
-portfolio.solution <- function(returns,model,covars,sigma0){
+portfolio.solution <- function(returns,model,covars,var){
   mean <- rowMeans(returns)
   a <- solve(covars$error.covar)
   b <- solve(covars$f.covar)
@@ -14,16 +14,18 @@ portfolio.solution <- function(returns,model,covars,sigma0){
   loadings <- model@loadings
   covar.inv <- a - a %*% t(loadings) %*% solve(b + loadings %*% a %*% t(loadings),loadings %*% a)
 
-  A <- t(mean) %*% covar.inv %*% mean
-  B <- t(rep(1,p)) %*% covar.inv %*% mean
-  C <- t(rep(1,p)) %*% covar.inv %*% rep(1,p)
-  D <- (C*sigma0^2 - 1)/(A*C - B^2) %>% sqrt()
+  sigma0 <- sqrt(var)
+  A <- t(mean)%*%covar.inv%*%mean %>% as.numeric()
+  B <- t(rep(1,p))%*%covar.inv%*%mean %>% as.numeric()
+  C <- t(rep(1,p))%*%covar.inv%*%rep(1,p) %>% as.numeric()
+  D <- (C*var -1)/(A*C - B^2) %>% sqrt
+  ones <- rep(1,p)
 
-  if (sigma0 * B <= sqrt(A)){
-    portfolio <- as.numeric(sigma0/(sqrt(A))) * (covar.inv %*% mean)
-  } else{
-    portfolio <- as.numeric(1/C) * covar.inv %*% rep(1,p) +
-      as.numeric(D)*(covar.inv%*%mean - as.numeric(B/C)*covar.inv%*%rep(1,p))
+  if (sigma0 * B / A <= 1){
+    portfolio <- (sigma0/A) * covar.inv %*% mean
+  }
+  else{
+    portfolio <- (1/C) * (covar.inv%*%ones) + D * (covar.inv%*%mean - (B/C) * covar.inv%*%ones)
   }
   return(portfolio)
 }
